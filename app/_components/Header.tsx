c00962c3f3c790baab33fa482dc965db83fa4047
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2025-08-14 04:56:39 +0000

    feat: setup project and create homepage

---
+++ app/_components/Header.tsx
@@ -0,0 +1,42 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const menuOptions = [
+  {
+    name: "Home",
+    path: "/"
+  },
+  {
+    name: "Pricing",
+    path: "/Pricing"
+  },
+  {
+    name: "Contact us",
+    path: "/Contact-us"
+  }
+]
+
+function Header() {
+  return (
+    <div className='flex justify-between items-center p-4'>
+      <div className='flex gap-2 items-center'>
+        <Image src={'/logo.svg'} alt='logo' width={30} height={30} />
+        <h2 className='font-bold text-2xl'>AlgoTrip</h2>
+      </div>
+      <div className="flex gap-8 items-center">
+        {menuOptions.map((menu) => (
+          <Link key={menu.path} href={menu.path}>
+            <h2 className='text-lg hover:scale-105 transition-all hover:text-primary'>{menu.name}</h2>
+          </Link>
+        ))}
+      </div>
+      <Button>
+        Get Started
+      </Button>
+    </div>
+  )
+}
+
+export default Header
---
+++ app/_components/Hero.tsx
@@ -0,0 +1,57 @@
+import { Button } from '@/components/ui/button'
+import { Textarea } from '@/components/ui/textarea'
+import { Globe, Send } from 'lucide-react'
+import React from 'react'
+
+const suggestions = [
+  {
+    title: "Create new trip",
+    icon: <Globe className='text-blue-400 w-5 h-5' />
+  },
+  {
+    title: "Inspire where you go",
+    icon: <Globe className='text-green-500 w-5 h-5' />
+  },
+  {
+    title: "Discover Hidden gems",
+    icon: <Globe className='text-orange-500 w-5 h-5' />
+  },
+  {
+    title: "Advanture Destination",
+    icon: <Globe className='text-yellow-600 w-5 h-5' />
+  }
+]
+
+function Hero() {
+  return (
+    <div className='mt-24 flex items-center justify-center'>
+      <div className='max-w-3xl w-full text-center '>
+        <h1 className='text-xl md:text-5xl font-bold '>Hey, I'm your personal <span className='text-primary'>Trip Planner</span></h1>
+        <p className='text-lg'>Tell me what you want, and I'll help you plan your perfect trip!</p>
+        <div>
+          <div className='border rounded-2xl p-4 relative'>
+            <Textarea 
+              placeholder='Create a trip for Paris from New York ' 
+              className='w-full h-28 bg-transparent border-none focus-visible:ring-0 shadow-none resize-none'
+            />
+            <Button size={'icon'} className='absolute bottom-6 right-6'>
+              <Send className='h-4 w-4'/>
+            </Button>
+          </div>
+        </div>
+        <div className='flex gap-5'>
+          {suggestions.map((suggestion, index) => {
+            return (
+              <div key={index} className='flex items-center gap-2 p-2 border rounded-full cursor-pointer hover:bg-primary hover:text-white transition-all'>
+                {suggestion.icon}
+                <h2 className='text-sm'>{suggestion.title}</h2>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
