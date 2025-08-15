ceb5e54c13f3f6a8812f25e432748d34e68bc56e
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2025-08-15 04:07:22 +0000

    feat: add HeroVideoDialog and PopularityCityList components; enhance Hero component with video dialog and new suggestions

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Hero from "./_components/Hero";
+import { PopularityCityList } from "./_components/PopularityCityList";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <PopularityCityList />
     </div>
   );
 }
